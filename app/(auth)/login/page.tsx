782400d3ccc5d0c431d7f1b3ecb63ed3cf17ee79
DSC-LABEL <dogesoundclub@gmail.com>
2023-12-24 06:24:56 +0900

    -

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,14 +14,17 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <Link href="/">
-        <a className={cn(
+      <Link
+        href="/"
+        className={cn(
           buttonVariants({ variant: "ghost" }),
           "absolute left-4 top-4 md:left-8 md:top-8"
-        )}>
+        )}
+      >
+        <>
           <Icons.chevronLeft className="mr-2 h-4 w-4" />
           Back
-        </a>
+        </>
       </Link>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
