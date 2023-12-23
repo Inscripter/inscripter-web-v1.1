c370a82fd6f49f54b883fbba5f40be80b26690a9
DSC-LABEL <dogesoundclub@gmail.com>
2023-12-24 05:29:59 +0900

    -

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
-import { withContentlayer } from "next-contentlayer"
+import { withContentlayer } from "next-contentlayer";
 
-import "./env.mjs"
+import "./env.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+  
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
@@ -12,6 +17,21 @@ const nextConfig = {
     appDir: true,
     serverComponentsExternalPackages: ["@prisma/client"],
   },
-}
 
-export default withContentlayer(nextConfig)
+  // CORS 헤더 설정 추가
+  async headers() {
+    return [
+      {
+        source: '/site.webmanifest', // site.webmanifest 파일에 적용
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*' // 모든 출처에서의 접근 허용
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default withContentlayer(nextConfig);
