7a55a15655bfb7ee204c21d749f305aca086ad04
DSC-LABEL <dogesoundclub@gmail.com>
2023-12-30 07:09:55 +0900

    revive api

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,7 +6,6 @@ import { ThemeProviderProps } from "next-themes/dist/types"
 import { Theme as RadixThemesProvider} from "@radix-ui/themes"
 import '@radix-ui/themes/styles.css';
 
-
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>
             <RadixThemesProvider>
